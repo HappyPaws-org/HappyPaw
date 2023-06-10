b6c5d1b9db90f4fa2fc3d77f551e10b4074a1d4f
Zhan <zhaslan@mail.ru>
2023-06-10 16:04:09 +0600

    feat: transfer elements to single components

--- client/src/components/service-card-item/ServiceCardsItem.js
+++ client/src/components/service-card-item/ServiceCardsItem.js
@@ -1,20 +1,16 @@
 import React from 'react'
 import cl from './ServiceCardsItem.module.scss'
-// import { useNavigate } from 'react-router-dom'
 
-const ServiceCardsItem = ({ img, title, content}) => {
-    // const navigate = useNavigate()
 
-    // function openPost(){
-    //   navigate("/posts/" + id)
-    // }
+const ServiceCardsItem = () => {
+    
     
       return (
         <div className={cl.card_item}>
-          <img src={img} alt="post-img" />
-          <h3>{title}</h3>
+          <img src={} alt="post-img" />
+          <h3>{}</h3>
           <p>
-            {content}
+            {}
           </p>
           <button >Подробнее &#8594;</button>
         </div>
--- client/src/components/service-card/ServiceCards.js
+++ client/src/components/service-card/ServiceCards.js
@@ -4,61 +4,14 @@ import cl from './ServiceCards.module.scss'
 import ServiceCardsItem from '../service-card-item/ServiceCardsItem'
 
 const ServiceCards = () => {
-    const [postIds, setPostIds] = useState([])
-    const [posts, setPosts] = useState([])
-
-    useEffect(() => {
-        getPostsIds();
-    }, [])
-
-    useEffect(() => {
-        console.log(postIds);
-    }, [postIds])
-
-
-    useEffect(() => {
-        getPost();
-    }, [])
-
-    useEffect(() => {
-        console.log(posts);
-    }, [posts])
-
-    async function getPostsIds(){
-        const response = await axios.get("http://localhost:5000/api/posts");
-        console.log("1" + response.data);
-        setPostIds(response.data)
-    }
-
-    async function getPost(postId){
-        const response = await axios.get("http://localhost:5000/api/posts/" + postId);
-        return response.data
-    }
-
-    postIds.forEach(async (postId) => {
-        try {
-            const post = await getPost(postId)
-            setPosts([ ...posts, post ]) 
-        } catch (err){
-            console.error("GetPost error: " + err);
-        }
-    })
     
-
-    const postItems = posts.map((post) => {
-        return <ServiceCardsItem img={post.postImage} 
-                        title={post.title} 
-                        content={post.content} 
-                        // id={post._id}
-                />
-    })
         
     return (
       <section className={cl.section2_services}>
         <div className={cl.container}>
           <h2>Наши услуги</h2>
           <div className={cl.service_cards}>
-                {postItems}
+                {}
           </div>
         </div>
       </section>
